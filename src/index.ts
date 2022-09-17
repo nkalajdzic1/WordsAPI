48aa501a8e7ce2c10cc65c8404cb3a4708c438ed
Nadir Kalajdzic <nkalajdzic1@etf.unsa.ba>
2022-09-17 19:59:18 +0200

    added procfile

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// import metadata for custom decorators
 import "reflect-metadata";
 
 import express, { Application } from "express";
@@ -5,10 +6,13 @@ import express, { Application } from "express";
 import { ENV, Injector } from "@config/index";
 import { Logger } from "@shared/classes/index";
 
+// create an instance of an server
 const app: Application = express();
 
+// inject all controllers
 Injector.injectControllers(app, `/api/${ENV.API_VERSION}`);
 
+// start server
 app.listen(ENV.PORT, () => {
   Logger.info(`Server is running at port ${ENV.PORT}`);
 });
